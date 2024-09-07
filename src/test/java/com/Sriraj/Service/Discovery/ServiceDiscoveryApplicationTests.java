87706dc60ed711a413af7ad87f48bcbce84370be
Sri Raj <srirajkumaryaramsetti10@gmail.com>
2024-09-06 20:58:22 -0500

    First commit

---
+++ src/test/java/com/Sriraj/Service/Discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sriraj.Service.Discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
